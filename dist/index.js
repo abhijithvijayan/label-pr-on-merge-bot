92602de6bf4b229972e742f35898c93b35410c33
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-02-14 21:11:44 +0530

    v0.4.0
    Signed-off-by: abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,25 @@ require('./sourcemap-register.js');module.exports =
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -16,22 +35,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(186));
+const core = __importStar(__nccwpck_require__(186));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const token = core_1.default.getInput('GITHUB_TOKEN');
-            const triggerLabel = core_1.default.getInput('label');
-            const postMergedLabel = core_1.default.getInput('post_merged_label');
+            const token = core.getInput('GITHUB_TOKEN');
+            const triggerLabel = core.getInput('label');
+            const postMergedLabel = core.getInput('post_merged_label');
             console.log({ token, triggerLabel, postMergedLabel });
-            core_1.default.setOutput('time', new Date().toTimeString());
+            core.setOutput('time', new Date().toTimeString());
         }
         catch (err) {
-            core_1.default.setFailed(err.message);
+            core.setFailed(err.message);
         }
     });
 }
