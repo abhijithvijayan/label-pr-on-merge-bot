b2fae80066a0926dbde651be9034e75c1144f532
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-02-14 20:54:30 +0530

    fix: use new build
    Signed-off-by: abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -7,22 +7,33 @@ require('./sourcemap-register.js');module.exports =
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core_1 = __importDefault(__nccwpck_require__(186));
-async function run() {
-    try {
-        const token = core_1.default.getInput('GITHUB_TOKEN');
-        const triggerLabel = core_1.default.getInput('label');
-        const postMergedLabel = core_1.default.getInput('post_merged_label');
-        console.log({ token, triggerLabel, postMergedLabel });
-        core_1.default.setOutput('time', new Date().toTimeString());
-    }
-    catch (err) {
-        core_1.default.setFailed(err.message);
-    }
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const token = core_1.default.getInput('GITHUB_TOKEN');
+            const triggerLabel = core_1.default.getInput('label');
+            const postMergedLabel = core_1.default.getInput('post_merged_label');
+            console.log({ token, triggerLabel, postMergedLabel });
+            core_1.default.setOutput('time', new Date().toTimeString());
+        }
+        catch (err) {
+            core_1.default.setFailed(err.message);
+        }
+    });
 }
 run();
 
