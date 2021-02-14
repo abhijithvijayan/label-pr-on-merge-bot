7f8f36c630914cc8ddb8ea69301f3f40dd326aa5
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-02-14 20:11:58 +0530

    fix: wrap in an async function
    Signed-off-by: abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>

--- source/index.ts
+++ source/index.ts
@@ -1,27 +1,23 @@
-#!/usr/bin/env node
-
 /**
- *  node-cli-ts-starter
+ *  label-pr-on-merge-bot
  *
  *  @author   abhijithvijayan <https://abhijithvijayan.in>
  *  @license  MIT License
  */
 
-import github from '@actions/github';
 import core from '@actions/core';
 
-try {
-  const token: string = core.getInput('GITHUB_TOKEN');
-  const triggerLabel: string = core.getInput('label');
-  const postMergedLabel: string = core.getInput('post_merged_label');
-  console.log({token, triggerLabel, postMergedLabel});
+async function run(): Promise<void> {
+  try {
+    const token: string = core.getInput('GITHUB_TOKEN');
+    const triggerLabel: string = core.getInput('label');
+    const postMergedLabel: string = core.getInput('post_merged_label');
+    console.log({token, triggerLabel, postMergedLabel});
 
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+    core.setOutput('time', new Date().toTimeString());
+  } catch (err) {
+    core.setFailed(err.message);
+  }
 }
+
+run();
